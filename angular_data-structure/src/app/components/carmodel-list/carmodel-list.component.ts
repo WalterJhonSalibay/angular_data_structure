65f0573e2fe8a2c1074b2d8e72fa97bc8564cc19
WalterJhonSalibay <salibaywaltz@gmail.com>
2024-10-16 02:07:51 +0800

    8.carmodellist

---
+++ angular_data-structure/src/app/components/carmodel-list/carmodel-list.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-carmodel-list',
+  standalone: true,
+  imports: [],
+  templateUrl: './carmodel-list.component.html',
+  styleUrl: './carmodel-list.component.css'
+})
+export class CarmodelListComponent {
+
+}
