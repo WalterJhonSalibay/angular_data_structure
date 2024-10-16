bf28d6baaf8e39a7f45183f34605abf14da3e1e5
WalterJhonSalibay <salibaywaltz@gmail.com>
2024-10-17 01:53:50 +0800

    26.stationarylist

---
+++ angular_data-structure/src/app/components/stationary-list/stationary-list.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-stationary-list',
+  standalone: true,
+  imports: [],
+  templateUrl: './stationary-list.component.html',
+  styleUrl: './stationary-list.component.css'
+})
+export class StationaryListComponent {
+
+}
