044dcd253d883d69b583b19da23039ad0009c36e
WalterJhonSalibay <salibaywaltz@gmail.com>
2024-10-17 02:19:01 +0800

    35.furniturelist

---
+++ angular_data-structure/src/app/components/furniture-list/furniture-list.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-furniture-list',
+  standalone: true,
+  imports: [],
+  templateUrl: './furniture-list.component.html',
+  styleUrl: './furniture-list.component.css'
+})
+export class FurnitureListComponent {
+
+}
